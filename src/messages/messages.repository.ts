efa8d388489f195452bf3e738d81e357dc93aa89
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-09-15 14:47:20 +0530

    creating service and repository

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,30 @@
+import { randomUUID } from 'crypto';
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = randomUUID();
+    messages[id] = {
+      id,
+      content,
+    };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+    return messages;
+  }
+}
